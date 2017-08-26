b0d50a4cf9454e07162f77f679b16da24407daa6
comp500 <comp500@users.noreply.github.com>
2017-08-26 11:43:39 +0100

    todo finish addinformation

---
+++ src/main/java/link/infra/ecmacraft/util/EcmaBlock.java
@@ -0,0 +1,49 @@
+package link.infra.ecmacraft.util;
+
+import java.util.List;
+
+import link.infra.ecmacraft.EcmaCraft;
+import link.infra.ecmacraft.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class EcmaBlock extends Block {
+	
+	public EcmaBlock(String itemID, Material blockMaterial, float hardness) {
+		super(blockMaterial);
+        setUnlocalizedName(EcmaCraft.MODID + "." + itemID);
+        setRegistryName(itemID);
+        setCreativeTab(ModItems.tab);
+        setHardness(hardness);
+	}
+
+	public EcmaBlock(String itemID, Material blockMaterial) {
+		super(blockMaterial);
+        setUnlocalizedName(EcmaCraft.MODID + "." + itemID);
+        setRegistryName(itemID);
+        setCreativeTab(ModItems.tab);
+	}
+	
+	@SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		if (extraTooltip) {
+			tooltip.add(I18n.format("tooltip." + EcmaCraft.MODID + ".item." + itemId + ".name"));
+		}
+	}
+
+}
