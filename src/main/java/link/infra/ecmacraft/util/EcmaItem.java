90e0fa96992beaea0ebf314ffa4ffe3743956fe2
comp500 <comp500@users.noreply.github.com>
2017-08-26 10:53:12 +0100

    Add ecma item util class

---
+++ src/main/java/link/infra/ecmacraft/util/EcmaItem.java
@@ -0,0 +1,44 @@
+package link.infra.ecmacraft.util;
+
+import java.util.List;
+
+import link.infra.ecmacraft.EcmaCraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class EcmaItem extends Item {
+	
+	private String extraTooltip;
+
+	public EcmaItem(String itemID, String tooltip) {
+		super();
+		setRegistryName(itemID);
+		setUnlocalizedName(EcmaCraft.MODID + "." + itemID);
+		extraTooltip = tooltip;
+	}
+	
+	public EcmaItem(String itemID) {
+		super();
+		setRegistryName(itemID);
+		setUnlocalizedName(EcmaCraft.MODID + "." + itemID);
+	}
+	
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		if (extraTooltip != null) {
+			tooltip.add(extraTooltip);
+		}
+	}
+	
+	@SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+	
+}
