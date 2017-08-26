3abb109dbb6fc2f83624b1e8c8034818d52e5921
comp500 <comp500@users.noreply.github.com>
2017-08-26 14:48:43 +0100

    Add motherboard base class

---
+++ src/main/java/link/infra/ecmacraft/motherboards/Motherboard.java
@@ -0,0 +1,15 @@
+package link.infra.ecmacraft.motherboards;
+
+import link.infra.ecmacraft.util.EcmaItem;
+
+public class Motherboard extends EcmaItem {
+
+	public Motherboard(String itemID) {
+		super(itemID);
+	}
+	
+	public Motherboard(String itemID, boolean tooltip) {
+		super(itemID, tooltip);
+	}
+
+}
