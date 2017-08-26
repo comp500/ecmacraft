7940f8cf573400a4d2eb68567b01157471810692
comp500 <comp500@users.noreply.github.com>
2017-08-26 11:37:04 +0100

    add a class

---
+++ src/main/java/link/infra/ecmacraft/cards/AddonCard.java
@@ -0,0 +1,15 @@
+package link.infra.ecmacraft.cards;
+
+import link.infra.ecmacraft.util.EcmaItem;
+
+public class AddonCard extends EcmaItem {
+
+	public AddonCard(String itemID) {
+		super(itemID);
+	}
+	
+	public AddonCard(String itemID, boolean tooltip) {
+		super(itemID, tooltip);
+	}
+
+}
